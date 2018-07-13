e74dc3174b0be440be9b90ba2fae2dedcc14df44
galaxy821 <hoshino208@naver.com>
2018-07-13 19:46:57 +0900

    영상 스트리밍 기능 추가

--- DingDong/app/src/main/java/com/project/hackerteam10/dingdong/MainActivity.java
+++ DingDong/app/src/main/java/com/project/hackerteam10/dingdong/MainActivity.java
@@ -1,13 +1,51 @@
 package com.project.hackerteam10.dingdong;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = (android.widget.VideoView) findViewById(R.id.videoView);
+        webView =(WebView) findViewById(R.id.webview);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        videoView.setVideoURI(Uri.parse("http://165.194.17.13:8080/?action=stream"));
+        videoView.requestFocus();
+        videoView.start();
+
+        webView.setPadding(50,0,50,0);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        String url ="http://165.194.17.13:8080/javascript_simple.html";
+        webView.loadUrl(url);
     }
 }
+
